b7522e11f2b40a2bf5f4fa141dc4fa206dc051dd
mike-chernavskikh <56829147+mike-chernavskikh@users.noreply.github.com>
2021-09-03 12:30:37 +0300

    Add files via upload

---
+++ projective_plane_cpp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main(void)
+{   
+    int n = 30;
+    int * ar;
+    ar = (int *)malloc(18 * n * n * n * n * n * n * sizeof(int));
+    int mod_z = 0;
+    for (int i = 0; i < 18 * n * n * n * n * n * n; i++) ar[i] = 0;
+
+
+    for (int x = 0; x < n; x++)
+        for (int y = 0; y < n; y++)
+            if (gcd(x, y) == 1)
+                {
+                    //z = x + y*\omega
+                    mod_z = x * x + x * y + y * y;
+                    for (int a = 1; a < n; a++)
+                        for (int b = 1; b < n; b++)
+                            for (int c = 1; c < n; c++)
+                                for (int d = 1; d < n; d++)
+                                    ar[mod_z * (a * b + a * c + a * d + b * c + b * d + c * d)] += 1;
+                }
+    for (int i = 0; i < 100; i ++) printf("%d, %d\n", i, ar[i]);
+    free(ar);
+return 0;
+}             
